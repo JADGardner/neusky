fix masking issue

reni_neus/data/nerfosr_cityscapes_dataparser.py
@@ -225,7 +225,7 @@ class NeRFOSRCityScapes(DataParser):
         elif self.config.mask_source == "cityscapes":
             panoptic_classes = load_from_json(Path(data) / "cityscapes_classes.json")
             classes = panoptic_classes["classes"]
-            colors = torch.tensor(panoptic_classes["colours"], dtype=torch.float32) / 255.0
+            colors = torch.tensor(panoptic_classes["colours"], dtype=torch.uint8)
             segmentation_filenames = _find_files(
                 f"{split_dir}/cityscapes_mask", exts=["*.png", "*.jpg", "*.JPG", "*.PNG"]
             )

reni_neus/data/reni_neus_dataset.py
@@ -21,6 +21,8 @@ from typing import Dict
 
 import numpy as np
 import torch
+from PIL import Image
+
 
 from nerfstudio.data.dataparsers.base_dataparser import DataparserOutputs, Semantics
 from nerfstudio.data.datasets.base_dataset import InputDataset
@@ -69,26 +71,19 @@ class RENINeuSDataset(InputDataset):
             metadata["normal"] = normal_image
 
         # handle mask
-        filepath = self.semantics.filenames[data["image_idx"]]
-        semantic_label, mask = get_semantics_and_mask_tensors_from_path(
-            filepath=filepath, mask_indices=self.mask_indices, scale_factor=self.scale_factor
+        mask = self.get_mask_from_semantics(
+            idx=data["image_idx"], semantics=self.semantics, mask_classes=self.semantics.mask_classes
         )
-        # mask shape is (H, W, 3, 1) we want H, W, 3
-        mask = mask.squeeze(-1).float()
-
-        if "mask" in data.keys():
-            mask = mask & data["mask"]
+        # mask shape is (H, W) we want H, W, 3
+        mask = (~mask).unsqueeze(-1).repeat(1, 1, 3).float()
 
         metadata["mask"] = mask
-        metadata["semantics"] = semantic_label
+        # metadata["semantics"] = semantic_label
 
         # handle foreground mask
-        _, fg_mask = get_semantics_and_mask_tensors_from_path(
-            filepath=filepath, mask_indices=self.fg_mask_indices, scale_factor=self.scale_factor
-        )
+        fg_mask = self.get_mask_from_semantics(idx=data["image_idx"], semantics=self.semantics, mask_classes=["sky"])
+        fg_mask = fg_mask.unsqueeze(-1).float()
 
-        fg_mask = (~fg_mask).squeeze()
-        fg_mask = fg_mask[:, :, 0:1]
         metadata["fg_mask"] = fg_mask
 
         return metadata
@@ -127,28 +122,19 @@ class RENINeuSDataset(InputDataset):
 
         return depth, normal
 
+    def get_mask_from_semantics(self, idx, semantics, mask_classes):
+        """function to get mask from semantics"""
+        filepath = semantics.filenames[idx]
+        pil_image = Image.open(filepath)
+        semantic_img = torch.from_numpy(np.array(pil_image, dtype="int32"))
+        mask = torch.zeros_like(semantic_img[:, :, 0])
+        combined_mask = torch.zeros_like(semantic_img[:, :, 0])
 
-class SemanticDataset(InputDataset):
-    """Dataset that returns images and semantics and masks.
-
-    Args:
-        dataparser_outputs: description of where and how to read input images.
-    """
-
-    def __init__(self, dataparser_outputs: DataparserOutputs, scale_factor: float = 1.0):
-        super().__init__(dataparser_outputs, scale_factor)
-        assert "semantics" in dataparser_outputs.metadata.keys() and isinstance(self.metadata["semantics"], Semantics)
-        self.semantics = self.metadata["semantics"]
-        self.mask_indices = torch.tensor(
-            [self.semantics.classes.index(mask_class) for mask_class in self.semantics.mask_classes]
-        ).view(1, 1, -1)
-
-    def get_metadata(self, data: Dict) -> Dict:
-        # handle mask
-        filepath = self.semantics.filenames[data["image_idx"]]
-        semantic_label, mask = get_semantics_and_mask_tensors_from_path(
-            filepath=filepath, mask_indices=self.mask_indices, scale_factor=self.scale_factor
-        )
-        if "mask" in data.keys():
-            mask = mask & data["mask"]
-        return {"mask": mask, "semantics": semantic_label}
+        for mask_class in mask_classes:
+            class_colour = semantics.colors[semantics.classes.index(mask_class)].type_as(semantic_img)
+            class_mask = torch.where(
+                torch.all(torch.eq(semantic_img, class_colour), dim=2), torch.ones_like(mask), mask
+            )
+            combined_mask += class_mask
+        combined_mask = combined_mask.bool()
+        return combined_mask

reni_neus/data/reni_neus_pixel_sampler.py
@@ -17,7 +17,8 @@ Code for sampling pixels.
 """
 
 import torch
-from typing import Dict
+from typing import Dict, Optional, Union
+from torchtyping import TensorType
 
 from nerfstudio.data.pixel_samplers import PixelSampler
 
@@ -25,6 +26,37 @@ from nerfstudio.data.pixel_samplers import PixelSampler
 class RENINeuSPixelSampler(PixelSampler):
     def __init__(self, num_rays_per_batch: int, keep_full_image: bool = False, **kwargs) -> None:
         super().__init__(num_rays_per_batch=num_rays_per_batch, keep_full_image=keep_full_image, **kwargs)
+
+    def sample_method(  # pylint: disable=no-self-use
+        self,
+        batch_size: int,
+        num_images: int,
+        image_height: int,
+        image_width: int,
+        mask: Optional[TensorType] = None,
+        device: Union[torch.device, str] = "cpu",
+    ) -> TensorType["batch_size", 3]:
+        """
+        Naive pixel sampler, uniformly samples across all possible pixels of all possible images.
+
+        Args:
+            batch_size: number of samples in a batch
+            num_images: number of images to sample over
+            mask: mask of possible pixels in an image to sample from.
+        """
+        if isinstance(mask, torch.Tensor):
+            nonzero_indices = torch.nonzero(mask[..., 0], as_tuple=False)
+            chosen_indices = torch.randint(0, len(nonzero_indices), (batch_size,))
+            height_width_indices = nonzero_indices[chosen_indices]  # shape (batch_size, 2)
+            image_index = torch.zeros(batch_size, 1, dtype=torch.long, device=device)  # shape (batch_size, 1)
+            indices = torch.cat((image_index, height_width_indices), dim=1)  # shape (batch_size, 3)
+        else:
+            indices = torch.floor(
+                torch.rand((batch_size, 3), device=device)
+                * torch.tensor([num_images, image_height, image_width], device=device)
+            ).long()
+
+        return indices
 
     def collate_image_dataset_batch_list(self, batch: Dict, num_rays_per_batch: int, keep_full_image: bool = False):
         """
@@ -63,7 +95,7 @@ class RENINeuSPixelSampler(PixelSampler):
                     num_rays_in_batch, 1, image_height, image_width, mask=batch["mask"][i], device=device
                 )
 
-                indices = torch.cat([torch.full((num_rays_in_batch, 1), i, device=device), indices], dim=-1)
+                indices[:, 0] = i
                 all_indices.append(indices)
                 all_images.append(batch["image"][i][indices[:, 1], indices[:, 2]])
 